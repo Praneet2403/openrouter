16ee72db830c29e8225a55727c5f99b3d14a3bd4
Praneet Kumar Dutta <duttapraneet@gmail.com>
2026-03-22 19:49:44 +0530

    all api end-points created

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from "elysia";
 
 import {app as authApp} from "./modules/auth"
+import {app as apiKeyApp} from "./modules/apiKeys"
 
-const app = new Elysia().use(authApp).listen(3000);
+const app = new Elysia()
+.use(authApp)
+.use(apiKeyApp)
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
